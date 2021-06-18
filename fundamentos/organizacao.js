4d4746c59893809f9a84f339bdc447bae927fb64
Gabriel Pires <gabriel.r.pires@outlook.com>
2021-06-18 13:09:50 -0300

    finalizando a primeira aula do curso

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log("sentença de código");
+
+// Como o javascript é organizado?
+// Ele é organizado em sentenças de códigos e em blocos de códigos
+{
+  {
+    console.log("olá");
+    console.log('mundo!');
+  }
+}
