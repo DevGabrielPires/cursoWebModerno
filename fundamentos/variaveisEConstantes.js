d45cfc70fb8d42ab07beaa8df4ce0762943963d8
Gabriel Pires <gabriel.r.pires@outlook.com>
2021-06-21 17:39:09 -0300

    finalizando aula sobre let,var e const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,14 @@
+var a = 3;
+let b = 4;
+
+var a = 30;
+// let b = 40; isso gera um erro pois no Js não se pode redeclarar let
+b = 40 // mas pode mudar o valor de dentro dela
+
+console.log(a, b)
+
+const c = 5
+
+// c = 50 isso gera um erro pois não se pode mudar o valor de uma constante
+
+console.log(c)
