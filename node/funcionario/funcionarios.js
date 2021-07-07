31710b2bfce00c556661fd2d297ea32cd53b8749
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-07-07 16:09:01 -0300

    finalizando aula Resolvendo exercicio

--- node/funcionario/funcionarios.js
+++ node/funcionario/funcionarios.js
@@ -1,7 +1,20 @@
 const url =  'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios  = require('axios')
 
+const chineses = f => f.pais === 'China';
+const mulheres = f => f.genero  === 'F';
+const menorSalario = (funcionario, funcionrioAtual) => {
+    return funcionario.salario < funcionrioAtual.salario ? funcionario : funcionrioAtual
+}
+
 axios.get(url).then(response => {
     const funcionarios = response.data
     console.log(funcionarios)
+
+    const funcionario = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+    
+    console.log(funcionario)
 })
