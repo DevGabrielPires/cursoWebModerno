76f77ef6aa1169af68b828d11c69a4ec4610ba10
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-10-27 22:37:35 -0300

    fizalizando aula 25

---
+++ fundamentos/inicio/dados_3.js
@@ -0,0 +1,13 @@
+let idade = 31;
+
+console.log(typeof 21);
+console.log(typeof idade);
+
+idade = 31.544;
+
+console.log(typeof idade);
+
+let estaChovendo = true;
+console.log(typeof estaChovendo);
+
+console.log(typeof 'estaChovendo');
