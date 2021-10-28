6f634e903c6ce2f9fa1ee1629b5bbe717fc6e62e
DevGabrielPires <gabriel.r.pires@outlook.com>
2021-10-27 22:28:21 -0300

    finalizando aula 24

---
+++ fundamentos/inicio/dados_2.js
@@ -0,0 +1,10 @@
+let preco = 19.9;
+let desconto = 0.4;
+
+console.log(preco * (1 - desconto));
+
+let nome = 'caderno';
+let categoria = 'papelaria';
+console.log(
+  `produto: ${nome}, categoria: ${categoria}, preço: ${preco}, desconto: ${desconto} `
+);
