8bb2844b8ff4386f0e28414d77314a33c1d2be5e
DevGabrielPires <gabriel.r.pires@outlook.com>
2022-07-30 10:24:14 -0300

    :sparkles: Feat: Start again

--- fundamentos/inicio/comentario.js
+++
@@ -1,9 +0,0 @@
-// console.log é usado para exibir algo na tela do computador
-console.log('passo #03');
-console.log('passo #01'); // mais um exemplo de console.log
-// console.log('passo #02');
-
-/*
-// ola tudo bem
-// como voce esta
-*/
--- fundamentos/inicio/desafio_area.js
+++
@@ -1,5 +0,0 @@
-const PI = 3.1415;
-const Raio = 10;
-
-const area = PI * (Raio * Raio);
-console.log(`Area: ${area} Metros`);
